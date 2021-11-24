52a75ea4e31f1029cc750efb382d27f6a479927e
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2021-11-24 17:04:12 +0000

    Added HTTP support

--- index.ts
+++ index.ts
@@ -1,46 +1,49 @@
 const { io } = require('socket.io-client');
+const { createServer } = require('http');
+
+createServer((req: any, res: any) => res.end('HTTP Server running.')).listen(8080);
 
 const { uri, auth: authorization } = require('./config');
 
 const WSClient = io(uri, {
-	auth: {
-		token: authorization,
-		client: 'RequestClient'
-	}
+    auth: {
+        token: authorization,
+        client: 'RequestClient'
+    }
 });
 
 console.log('[WS REQ] Trying to connect to the RequestManager WebSocket...');
 
 WSClient.on('connect_error', (error: string) => {
-	console.log(
-		`[WS REQ] Error while trying to connect to the RequestManager Websocket! (${error})`
-	);
+    console.log(
+        `[WS REQ] Error while trying to connect to the RequestManager Websocket! (${error})`
+    );
 });
 
 WSClient.on('connect', () => {
-	console.log(
-		`[WS REQ] Connected to the RequestManager WebSocket! (${WSClient.id})`
-	);
+    console.log(
+        `[WS REQ] Connected to the RequestManager WebSocket! (${WSClient.id})`
+    );
 });
 
 WSClient.on('disconnect', (reason: any) => {
-	console.log(
-		`[WS REQ] Disconnected from the RequestManager Websocket. (${reason})`
-	);
+    console.log(
+        `[WS REQ] Disconnected from the RequestManager Websocket. (${reason})`
+    );
 });
 
 function wsRequest(event: string, data: any) {
-	return new Promise((resolve, reject) => {
-		WSClient.emit(event, data, (resp: any) => {
-			resolve(
-				resp.map((response: any) => {
-					if (!response.status || response.status != 'success') {
-						return reject(new Error(response.text));
-					}
+    return new Promise((resolve, reject) => {
+        WSClient.emit(event, data, (resp: any) => {
+            resolve(
+                resp.map((response: any) => {
+                    if (!response.status || response.status != 'success') {
+                        return reject(new Error(response.text));
+                    }
 
-					return response.response;
-				})
-			);
-		});
-	});
+                    return response.response;
+                })
+            );
+        });
+    });
 }
